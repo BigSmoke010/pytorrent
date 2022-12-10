56f6e9dd79c6e05a6868e946149afb4dc27194bb
BigSmoke010 <behirimohamedcherif0@gmail.com>
2022-12-10 21:15:23 +0100

    initial commit

---
+++ torrent.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter.ttk import *
+import libtorrent as lt
+import os
+import time
+
+root = Tk()
+s = lt.session()
+txtvar = StringVar()
+
+submitmagnetentry = Entry(root, width=50)
+submitmagnetentry.grid(row=0,column=1)
+
+def getmagnet():
+    txtvar.set('amosh')
+    paramtrs = {
+    'url':submitmagnetentry.get(),
+    'save_path' : 'downloads/',
+    'storage_mode' : lt.storage_mode_t(2)
+    }
+    handle = s.add_torrent(paramtrs)
+    while not handle.has_metadata:
+        time.sleep(1)
+    while handle.status().state != lt.torrent_status.seeding:
+        se = handle.status()
+        txtvar.set('Progress ' + str(se.progress * 100) + '\n' +'Seeds :' + str(se.num_seeds)+ '\n' + 'peers :' + str(se.num_peers) + '\n' + 'download rate : ' + str(se.download_rate / 1000))
+        print('Progress ' + str(se.progress * 100) + '\n' +'Seeds :' + str(se.num_seeds)+ '\n' + 'peers :' + str(se.num_peers) + '\n' + 'download rate : ' + str(se.download_rate / 1000))
+        time.sleep(2)
+
+submitmagnet = Button(root, text='magnet', command=getmagnet)
+submitmagnet.grid(row=0,column=0)
+lbox = Listbox(root, width=25, height=25)
+lbox.grid(row=1,column=0)
+
+status = Label(root, textvariable=txtvar)
+status.grid(row=1,column=1)
+txtvar.set('')
+for i in os.listdir('downloads/'):
+    lbox.insert('end', i)
+
+root.mainloop()
+
